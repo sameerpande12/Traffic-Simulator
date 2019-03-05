633b1bd56dce61c13ec053d441938fbbeaa1664c
Sameer <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-03-05 13:07:07 +0530

    added changeVelocity and changePosition without lane changing.

--- main.cpp
+++ main.cpp
@@ -11,11 +11,13 @@ references- https://www.geeksforgeeks.org/stdany-class-in-c/
 
 using namespace std;
 
+float getRandom();
+
 class Road{
   int id;//
   int length;//
   int width;// make them of type final
-  int** road_matrix;
+  bool** road_matrix;
 }
 
 class Vehicle{
@@ -44,21 +46,31 @@ public:
   int velocity[2]={0,0};//velocity[0] is x_velocity and velocity[1] is y_velocity
   int lane_no;//represents lane on which top right corner of vechile is
   Road on_road;//road on which vehicle is moving
-  int pos[2] = {1,1};//initial position (x,y) coordinates
+  int pos[2] = {0,0};//initial position (x,y) coordinates
 
+  bool** road_matrix = on_road.road_matrix;
 
-  void changeVelocity(){
-
-
-
+  void changeVelocity(){/*lane changing and overtaking not yet coded */
+    int max_xvel = velocity[0]+max_acceleration;
+    for(int i = 0; i<width ;i++){//ensures that driver takes maximum velocity possible so as to avoid collision
+        for(int j = 1; j <= max_xvel ; j++){
+           if(road_matrix[j+pos[0]][i+pos[1]] == true){
+             max_xvel = j-1;
+             continue;
+           }
+        }
+    }
+   velocity[0]= max_xvel;
+   velocity[1]= 0;
   }
 
-  void changePosition(){
-
-
-
+  void changePosition(){// changing position in unit times
+   pos[0] = pos[0]  + velocity[0];
+   pos[1] = pos[1]  + velocity[1];
   }
+}
 
+void updateRoad(){//function to update road matrix
 
 }
 
